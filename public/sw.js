425154324b471bb717b28e859eb25f0a79e93f09
anytime-sync <anytime.sync@gmail.com>
2026-05-30 11:42:14 +0800

    Update service worker version for Bump SW_VERSION to fl-v54 to evict stale bundlesquick add feature

--- public/sw.js
+++ public/sw.js
@@ -10,7 +10,7 @@
  *
  * Increment SW_VERSION when shipping changes so old caches get evicted.
  */
-const SW_VERSION = "fl-v53-2026-05-24-today-add-task";
+const SW_VERSION = "fl-v54-2026-05-24-quickadd-paste";
 const SHELL_CACHE = `shell-${SW_VERSION}`;
 const RUNTIME_CACHE = `runtime-${SW_VERSION}`;
 
